34c70de7db2bbf4d42c993963eef5005681156fe
jva23 <jvanarsd23@gmail.com>
2015-06-23 22:38:26 -0400

    Added form templates to add a new user group and to join an existing group

--- gifturlist.js
+++ gifturlist.js
@@ -1,8 +1,9 @@
 myGifts = new Meteor.Collection("myGifts");
 
-
 if (Meteor.isClient) {
-  Template.body.helpers({
+
+
+    Template.body.helpers({
     myGifts: function () {
         return myGifts.find({owner: Meteor.userId()}, {sort: {createdAt: -1}});
     }
@@ -30,7 +31,55 @@ if (Meteor.isClient) {
         "click .delete": function() {
             myGifts.remove(this._id);
         }
-    })
+    });
+
+    Session.setDefault('createGroupVisible',false);
+
+    Template.createGroup.events({
+        "click .toggle-create-group": function () {
+            Session.set("createGroupVisible", true)
+        }
+    });
+
+    Template.createGroup.helpers({
+        showCreateGroup:function(){
+            var showCreateGroupVisible = Session.get('createGroupVisible');
+            if(showCreateGroupVisible === true){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    });
+
+    //to set the session to false on refresh.
+    Template.createGroup.destroyed = function(){
+        Session.set('createGroupVisible',false)
+    };
+
+    Session.setDefault('joinGroupVisible',false);
+
+    Template.joinGroup.events({
+        "click .toggle-join-group": function () {
+            Session.set("joinGroupVisible", true)
+        }
+    });
+
+    Template.joinGroup.helpers({
+        showJoinGroup:function(){
+            var show = Session.get('joinGroupVisible');
+            if(show === true){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    });
+
+    //to set the session to false on refresh.
+    Template.joinGroup.destroyed = function(){
+        Session.set('joinGroupVisible',false)
+    };
 
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_AND_EMAIL"
